71f836e78455de07814c43ca98943c9d45a85d5d
Kanelbollen <136345631+Kanelbollen@users.noreply.github.com>
2023-06-12 16:16:53 +0200

    Update Projekt1.py
    Den opdaterede

--- Projekt1.py
+++ Projekt1.py
@@ -44,10 +44,87 @@ def dataLoad(filename):
     #data = np.array(data)
     return data
 #print(dataLoad("C:/Users/Bruger/OneDrive - Danmarks Tekniske Universitet/DTU/2. semester/02633 Introduktion til Programmering og Databehandling/Data files for projects/Data files for projects/Bacteria/test.txt"))
+data = dataLoad("C:/Users/Bruger/OneDrive - Danmarks Tekniske Universitet/DTU/2. semester/02633 Introduktion til Programmering og Databehandling/Data files for projects/Data files for projects/Bacteria/test.txt")
+#print(data)
+#print(len(data))
+#print(len(data[0]))
 
 #data = dataLoad("C:/Users/Bruger/OneDrive - Danmarks Tekniske Universitet/DTU/2. semester/02633 Introduktion til Programmering og Databehandling/Data files for projects/Data files for projects/Bacteria/test.txt")
+import matplotlib.pyplot as plt 
+def dataPlot(data):
+    #Sorting data into categories
+    Temp = np.ravel(data[0,:])
+    Growth = np.ravel(data[1,:])
+    Bact = np.ravel(data[2,:])
+    Salmonella = []
+    xSal = []
+    Bacillus = []
+    xBac = []
+    Listeria = []
+    xList = []
+    Brochothrix = []
+    xBroc = []
+    #Plotting number of bacteria in a histogram
+    plt.hist(Bact,4,color = "magenta")
+    plt.title("Number of bacteria")
+    plt.show()
+    #Sorting data by bacteria for plot data
+    for i in range(len(Bact)):
+        if Bact[i] == 1:
+            Salmonella.append(Growth[i])
+            xSal.append(Temp[i])
+        if Bact[i] == 2:
+            Bacillus.append(Growth[i])
+            xBac.append(Temp[i])
+        if Bact[i] == 3:
+            Listeria.append(Growth[i])
+            xList.append(Temp[i])
+        if Bact[i] == 4:
+            Brochothrix.append(Growth[i])
+            xBroc.append(Temp[i])
+    #Plotting growth rate by temperature for the four types of bacteria
+    plt.plot(xSal,Salmonella,"b.")
+    plt.plot(xBac,Bacillus,".", color = "orange")
+    plt.plot(xList,Listeria,"r.")
+    plt.plot(xBroc,Brochothrix,"g.")
+    plt.title("Growth rate by temperature")
+    plt.xlabel("Temperature")
+    plt.ylabel("Growth Rate")
+    plt.xlim([10,60])
+    plt.ylim([0,1.1])
+    plt.legend(["Salmonella","Bacillus","Listeria","Bronchothrix"],loc="upper right")
+    plt.show()
+print(dataPlot(data))
+   #%% 
+    Salmonella = []
+    Bacillus = []
+    Listeria = []
+    Brochothrix = []
+    for i in range(len(data[0])):
+        if data[i] == 1:
+            Salmonella.append(data[:,i])
+        if data[i] == 2:
+            Bacillus.append(data[:,i])
+        if data[i] == 3:
+            Listeria.append(data[:,i])
+        if data[i] == 4:
+            Brochothrix.append(data[:,i])
+    return Bb
+print(dataPlot(data))
+#%%
+    Temperature = np.ravel(data[0,:])
+    GrowthRate = np.ravel(data[1,:])
+    plt.plot(Temperature,GrowthRate)
+    plt.title("")
+    plt.xlabel("")
+    plt.ylabel("")
+    plt.xlim([10,60])
+    plt.ylim([0,10])
+    plt.show()
 
 
+
+#%%
 val = ["Mean Temperature","Mean Growth rate","Std Temperature","Std Growth rate", "Rows", "Mean Cold Growth rate", "Mean Hot Growth rate"]
 
 statistic = val[1]
